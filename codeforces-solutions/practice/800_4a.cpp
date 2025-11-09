32cc7dae93f5d9712a437ad6364b333151b361fd
Christian Ch. <71399835+cchung2020@users.noreply.github.com>
2025-11-09 00:44:48 -0500

    Add project files.

---
+++ codeforces-solutions/practice/800_4a.cpp
@@ -0,0 +1,18 @@
+#include "800_4a.h"
+
+#include <iostream>
+#include <print>
+
+using namespace std;
+
+void solve_4a() {
+	int num;
+	cin >> num;
+
+	if (num > 2 && num % 2 == 0) {
+		println("YES");
+	}
+	else {
+		println("NO");
+	}
+}

836477bde6e090e6c61dd8c475cfc098ed0e4b94
Christian Ch. <71399835+cchung2020@users.noreply.github.com>
2025-11-15 18:19:14 -0500

    problem 479a

--- codeforces-solutions/practice/1000_479a.h
+++ codeforces-solutions/practice/1000_479a.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <iostream>
+#include <print>
+
+using namespace std;
+
+void solve_479a() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int answer = max(a + b + c, max((a + b) * c, max(a * (b + c), a * b * c)));
+
+	std::println("{}", answer);
+}

338c218299884f32b04fb05c182bb57714759e66
Christian Ch. <71399835+cchung2020@users.noreply.github.com>
2025-11-15 22:09:06 -0500

    fixed error in 131a

---
+++ codeforces-solutions/1000_131a.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <print>
+#include <string>
+#include <algorithm>
+#include <ranges>
+
+using namespace std;
+using std::ranges::views::drop;
+
+void solve_131a() {
+	string word;
+	cin >> word;
+
+	if (ranges::all_of(word, ::isupper)) {
+		ranges::transform(word, word.begin(), ::tolower);
+	}
+	else if (ranges::all_of(word | drop(1), ::isupper)) {
+		word[0] = toupper(word[0]);
+		ranges::transform(word | drop(1), word.begin() + 1, ::tolower);
+	}
+
+	println("{}", word);
+}
